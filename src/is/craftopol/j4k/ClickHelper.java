317e9c44b4821432454a5c79272c33839a6b606a
Trent Davies <trentwdb@gmail.com>
2011-12-04 02:29:56 +0000

    derp
    git-svn-id: file:///Users/mlaux/Downloads/svn/j4k@6 dabb5e64-7a04-451e-a6d5-d27990723237

--- src/is/craftopol/j4k/ClickHelper.java
+++ src/is/craftopol/j4k/ClickHelper.java
@@ -1,21 +1,25 @@
 package is.craftopol.j4k;
 
+import java.awt.AlphaComposite;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
 public class ClickHelper extends Canvas {
 	private int lastX;
 	private int lastY;
-	
-	private ArrayList<Integer> xPoints = new ArrayList<Integer>();
-	private ArrayList<Integer> yPoints = new ArrayList<Integer>();
+	private int running;
+	private double currentX[] = new double[200];
+	private double currentY[] = new double[200];
+	private double moveX[] = new double[200];
+	private double moveY[] = new double[200];
+	private int scale = 4;
 	
 	public ClickHelper() {
 		addMouseListener(new Mouse());
@@ -25,11 +29,42 @@ public class ClickHelper extends Canvas {
 	public void paint(Graphics g) {
 		super.paint(g);
 		
-		g.setColor(Color.black);
-		g.drawString("(" + lastX + ", " + lastY + ")", 5, 15);
-		
-		for(int k = 0; k < xPoints.size(); k++) {
-			g.fillRect(xPoints.get(k) - 3, yPoints.get(k) - 3, 6, 6);
+		if (running>0) {
+			if (running==4000) {	
+				for(int i = 0; i < currentX.length; i++) {
+					int pos = ((int)(Math.random()*2))*2-1;
+					currentX[i]=pos*25*scale + lastX;
+					currentY[i]=lastY;
+					
+					moveX[i]=Math.random()*0.5*pos*(scale/2);
+					moveY[i]=Math.random()*0.5*(scale/2);
+					
+//					AlphaComposite alpha = AlphaComposite.getInstance(AlphaComposite.SRC);
+					g.fillOval((int)(currentX[i] - 3*scale), (int)(currentY[i] - 3*scale), 6*scale, 6*scale);
+					Color color = new Color(0,0,0,0);
+					((Graphics2D) g).setPaint(color);
+//					((Graphics2D) g).setComposite(alpha);		
+				}
+			}
+			for(int i = 0; i < currentX.length; i++) {
+				//when implementing replace currentX.length with the actual count (less data?)
+				moveX[i]-=moveX[i]/200;
+				moveY[i]-=moveY[i]/200 + 0.005;
+				
+				currentX[i]+=moveX[i];
+				currentY[i]-=moveY[i];
+				
+				if (currentY[i]>=597) {
+					currentY[i]-=(currentY[i]-597);
+					moveY[i]=0;
+				}
+				
+				g.fillOval((int)(currentX[i] - 3*scale), (int)(currentY[i] - 3*scale), 6*scale, 6*scale);
+			}
+			
+			running--;
+			
+			repaint();
 		}
 	}
 	
@@ -47,8 +82,8 @@ public class ClickHelper extends Canvas {
 			lastX = e.getX();
 			lastY = e.getY();
 			
-			xPoints.add(lastX);
-			yPoints.add(lastY);
+			running=4000;
+			
 			
 			repaint();
 			
