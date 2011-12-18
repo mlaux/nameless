771c439764f71b640ac496609aee993c93d2feac
Trent Davies <trentwdb@gmail.com>
2011-12-18 03:57:44 +0000

    git-svn-id: file:///Users/mlaux/Downloads/svn/j4k@43 dabb5e64-7a04-451e-a6d5-d27990723237

--- src/is/craftopol/j4k/Triangle.java
+++ src/is/craftopol/j4k/Triangle.java
@@ -72,7 +72,7 @@ public class Triangle implements Item {
 		int width = x2-x1;
 		
 		double percAcross = (x - x1) / ((double) width);
-		if (y <= minY && y >= (minY - ((double) height) * ((double) percAcross))) {
+		if (y <= minY + 2000 && y >= (minY - ((double) height) * ((double) percAcross))) {
 			if (percAcross <= 1 && percAcross >=0) {
 				//did it on a different line so you'll get it. because of how I did percAcross it's always in between 0 and 1 if your mouse is inside the triangle.
 				System.out.println("true");
