31306941994132107125900cff83a0343764b035
Trent Davies <trentwdb@gmail.com>
2012-02-10 00:33:54 +0000

    git-svn-id: file:///Users/mlaux/Downloads/svn/j4k@69 dabb5e64-7a04-451e-a6d5-d27990723237

--- src/is/craftopol/j4k/Button.java
+++ src/is/craftopol/j4k/Button.java
@@ -3,32 +3,35 @@ package is.craftopol.j4k;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class Button extends Item {
-	public static final int HEIGHT = 8;
+	public static final int THICKNESS = 8;
 	
 	private int xChange;
 	private int yChange;
 	
-	public int width;
+	public int x2;
+	public int y2;
 	
 	public Button() {
 		
 	}
 	
-	public Button(int x, int y, int w, Animation a) {
-		this.x1 = x;
-		this.y1 = y;
-		this.width = w;
+	public Button(int x1, int y1, int x2, int y2, Animation a) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
 		animation = a;
 	}
 
 	public String serialize() {
-		return "" + (char) Item.TYPE_BUTTON + (char) x1 + (char) y1 + (char) width;
+		return "" + (char) Item.TYPE_BUTTON + (char) x1 + (char) y1 + (char) x2 + (char) y2;
 	}
 
 	public Dimension getSize() {
-		return new Dimension(width, HEIGHT);
+		return new Dimension(Math.abs(x2 - x1), Math.abs(y2 - y1));
 	}
 
 	public void render(Graphics g) {
@@ -37,23 +40,36 @@ public class Button extends Item {
 		else 
 			g.setColor(Color.black);
 		
-		int[] xp = { x1, x1 + 8, x1 + 8 };
-		int[] yp = { y1, y1, y1 - 4 };
+		boolean flip = false;
+		double angle = Math.atan2(y2 - y1, x2 - x1);
+		int distance = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		final int MAX_BUTTON_SIZE = 8;
 		
-		g.fillPolygon(xp, yp, 3);
+		x2 = x1 + distance;
+		System.out.println(distance);
+		//if (x2 - 16 - MAX_BUTTON_SIZE < x1) {
+		///	x2 = x1 + 16 + MAX_BUTTON_SIZE;
+		//}
 		
-		xp = new int[] { x1 + width + 16, x1 + width + 8, x1 + width + 8 };
-		yp = new int[] { y1, y1, y1 - 4 };
-		g.fillPolygon(xp, yp, 3);
-		g.fillRect(x1 + 8, y1 - 8 , width, HEIGHT);
+		int[] xp1 = {x1, x1 + 8, x1 + 8};
+		int[] yp1 = {y1, y1, y1 - 4};
+		
+		int[] xp2 = {x2, x2 - 8, x2 - 8};
+		int[] yp2 = {y1, y1, y1 - 4};
+		
+		((Graphics2D) g).rotate(angle, x1, y1);
+		g.fillPolygon(xp1, yp1, 3);
+		g.fillRect(x1 + 8, y1 - 8, (x2 - x1) - 16, 8);
+		g.fillPolygon(xp2, yp2, 3);
+		((Graphics2D) g).rotate(-angle, x1, y1);
 		
 		if(animation != null)
 			animation.render(g);
 	}
 
 	public void setPosition(int x, int y) {
-		this.x1 = x;
-		this.y1 = y;
+		this.x2 = (this.x2 - this.x1) + x;
+		this.y2 = (this.y2 - this.y1) + y;
 	}
 
 	public void placeItemStart(Cursor cursor) {
@@ -62,10 +78,10 @@ public class Button extends Item {
 	}
 
 	public void placeItemDrag(Cursor cursor) {
-		width = cursor.getGridX() - x1 - 16;
-		if (width < 0) {
-			width = 0;
-		}
+		double angle = Math.atan2(y2, x2);
+		
+		x2 = cursor.getGridX();
+		y2 = cursor.getGridY();
 	}
 
 	public void setThickness(int thickness) {
@@ -77,11 +93,37 @@ public class Button extends Item {
 	}
 
 	public boolean contains(int x, int y) {
-		return x >= this.x1 && y <= this.y1 && x <= this.x1 + this.width + 32 && y >= this.y1 - HEIGHT;
+		return distanceTo(x, y) < Math.max(5, THICKNESS);
 	}
 
+	public int distanceTo(int px, int py) {
+		int vx = (int) (x2 - x1), vy = (int) (y2 - y1);
+		int wx = (int) (px - x1), wy = (int) (py - y1);
+		
+		int c1 = vx * wx + vy * wy;
+		if(c1 <= 0) {
+			// to the left
+			return (int) Math.sqrt(wx * wx + wy * wy);
+		}
+		
+		int c2 = vx * vx + vy * vy;
+		if(c2 <= c1) {
+			// to the right
+			int dx = (int) (px - x2), dy = (int) (py - y2);
+			return (int) Math.sqrt(dx * dx + dy * dy);
+		}
+		
+		double b = (double) c1 / c2;
+		int bx = (int) (x1 + (b * vx));
+		int by = (int) (y1 + (b * vy));
+		
+		int dx = px - bx, dy = py - by;
+		// on top or below
+		return (int) Math.sqrt(dx * dx + dy * dy);
+	}
+	
 	public Item clone() {
-		return new Button(this.x1, this.y1, this.width, this.animation);
+		return new Button(this.x1, this.y1, this.x2, this.y2, this.animation);
 	}
 
 	public void cloneItemDrag(Cursor cursor) {
